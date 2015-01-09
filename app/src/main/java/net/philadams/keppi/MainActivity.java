aea18fad03be7a31b38d186e0c449caa7d7df744
Phil Adams <philadams.net@gmail.com>
2015-01-09 18:41:26 -0500

    ensuring bluetooth turned on

--- app/src/main/java/net/philadams/keppi/MainActivity.java
+++ app/src/main/java/net/philadams/keppi/MainActivity.java
@@ -1,5 +1,9 @@
 package net.philadams.keppi;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +11,28 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+  private static final int REQUEST_ENABLE_BT = 0;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    ensureBluetoothIsEnabled();
+  }
+
+  private void ensureBluetoothIsEnabled() {
+    final BluetoothManager bluetoothManager =
+        (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+    if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+      Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+      startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
   }
 
   @Override
